32c97d8679763a2dac3a13fd1a6b380980d24d55
MiaoYu1 <emailyumiao@gmail.com>
2015-11-05 19:06:14 -0500

    update 11/05/2015-7pm
    Update predict function

--- MATLAB prototype/Datum.m
+++ MATLAB prototype/Datum.m
@@ -1,16 +1,28 @@
-classdef Datum
+classdef Datum < handle
     
     %{
         
     %}
     
     properties
-        
+        x % position
+        z % value
+        i % unique ID
     end
     
     methods
-        function
-            
+        function obj = Datum(x,z,i)
+            obj.x = x;
+            obj.z = z;
+            obj.i = i;
+        end
+        
+        function x = getX(self)
+            x = self.x;
+        end
+        
+        function z = getZ(self)
+            z = self.z;
         end
     end
     
